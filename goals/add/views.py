c2caee05836feacc05861c6196da638df74d26f5
Ramil Mavliutov <stirk2281337@gmail.com>
2023-02-22 22:52:47 +0500

    Доделал формы редактирования и создание цели

--- goals/add/views.py
+++ goals/add/views.py
@@ -1,4 +1,23 @@
 from django.shortcuts import render
+from browse.forms import GoalForm
+from browse.models import Goal
 
 def add(request):
-    return render(request, 'add/add.html')
+    status = ''
+    data = {}
+    if request.method == 'POST':
+        form = GoalForm(request.POST)
+        if form.is_valid():
+            goal = Goal(owner_id=request.user.id, name=request.POST.get('name'), 
+                        description=request.POST.get('description'), block=request.POST.get('block'),
+                        quarter=request.POST.get('quarter'), weight=request.POST.get('weight'),
+                        current=False, planned=True if request.POST.get('planned') == 'on' else False,
+                        chat={"chat": []}, history={"history": []})
+            goal.save()
+        else:
+            data = {'name': request.POST.get('name'), 'description': request.POST.get('description'),
+                    'block': request.POST.get('block'), 'quarter': request.POST.get('quarter'),
+                    'weight': request.POST.get('weight'), 'planned': request.POST.get('planned')}
+            status = 'Вес должен быть в диапазоне от 0 до 100'
+    form = GoalForm(initial=data)
+    return render(request, 'add/add.html', {'form': form, 'status': status})
--- goals/browse/views.py
+++ goals/browse/views.py
@@ -2,23 +2,27 @@ from django.shortcuts import render
 from .models import Goal
 from .forms import GoalForm, ChatForm
 from django.contrib.auth.models import User
-import json
 import datetime
-from datetime import date, datetime
+from datetime import datetime
 
 def get_time() -> str:
     return datetime.today().strftime('%d-%m-%Y') + ' ' + datetime.now().strftime("%H:%M")
 
 def update_history(goal, request):
-    new_data = {'name': request.POST.get('name')}
-    old_data = {'name': goal.name}
-    translator = {'name': 'Название'}
+    new_data = {'name': request.POST.get('name'), 'description': request.POST.get('description'),
+                'block': request.POST.get('block'), 'quarter': request.POST.get('quarter'),
+                'weight': request.POST.get('weight'), 'planned': True if request.POST.get('planned') == 'on' else False}
+
+    old_data = {'name': goal.name, 'description': goal.description, 'block': goal.block,
+                'quarter': goal.quarter, 'weight': goal.weight, 'planned': goal.planned}
+
+    translator = {'name': 'Название', 'description': 'Описание', 'block': 'Блок',
+                  'quarter': 'Квартал', 'weight': 'Вес', 'planned': 'Запланированная'}
 
     for i in new_data:
         if old_data[i] != new_data[i]:
             goal.history['history'].append({'id': request.user.id, 'time': get_time(), 'field': translator[i], 'last': old_data[i], 'now': new_data[i]})
     goal.save(update_fields=['history'])
-
 
 def browse(request):
     data = Goal.objects.all()
@@ -31,13 +35,23 @@ def editing(request, goal_id):
         if request.user.is_authenticated and request.user.id == goal.owner_id or \
         request.user.is_superuser or request.user.groups.all()[0] == User.objects.get(id=goal.owner_id).groups.all()[0] \
         and request.user.has_perm('browse.change_goal'):
-            status = 'Успешно'
-            update_history(goal, request)
-            goal.name = request.POST.get('name')
-            goal.save(update_fields=['name'])
+            form = GoalForm(request.POST)
+            if form.is_valid():
+                status = 'Успешно'
+                update_history(goal, request)
+                goal.name = request.POST.get('name')
+                goal.description = request.POST.get('description')
+                goal.block = request.POST.get('block')
+                goal.quarter = request.POST.get('quarter')
+                goal.weight = request.POST.get('weight')
+                goal.planned = True if request.POST.get('planned') == 'on' else False
+                goal.save(update_fields=['name', 'description', 'block', 'quarter', 'weight', 'planned'])
+            else:
+                status = 'Вес должен быть в диапазоне от 0 до 100'
         else:
             status = 'У вас недостаточно прав'
-    form = GoalForm(initial={'name': goal.name})
+    form = GoalForm(initial={'name': goal.name, 'description': goal.description, 'block': goal.block,
+                    'quarter': goal.quarter, 'weight': goal.weight, 'planned': goal.planned, })
     return render(request, 'browse/editing.html', {'data': goal, 'form': form, 'status': status})
 
 def chatting(request, goal_id):
