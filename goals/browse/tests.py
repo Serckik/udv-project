cc3f1f464be32d6119b4ec3fde9f01871adae4f0
Denis <106258306+xPotat0@users.noreply.github.com>
2023-02-23 23:16:35 +0500

    Add files via upload

--- goals/browse/tests.py
+++ goals/browse/tests.py
@@ -1,3 +1,58 @@
-from django.test import TestCase
+import django.db.models.fields
+from django.core.exceptions import ValidationError
+from django.test import TestCase, TransactionTestCase
+from .models import Goal
+from django.db import transaction
 
-# Create your tests here.
+
+class Settings(TransactionTestCase):
+
+    @classmethod
+    def setUpClass(cls):
+        super().setUpClass()
+        # Создаём тестовую запись Goal
+        with transaction.atomic():
+            cls.Goal = Goal.objects.create(
+                owner_id=1,
+                name="Objective",
+                description="This objective is secure",
+                block="Подбр",
+                quarter="1",
+                weight=20,
+                current=True,
+                planned=False,
+            )
+
+    @classmethod
+    def tearDown(self):
+        with transaction.atomic():
+            super().tearDownClass()
+
+
+class GoalModelTests(Settings):
+    def test_owner_id(self):
+        self.assertEqual(self.Goal.owner_id, 1)
+
+    def test_name(self):
+        self.assertEqual(self.Goal.name, "Objective")
+
+    def test_description_type(self):
+        self.assertEqual(self.Goal._meta.get_field("description").verbose_name, "Подробности")
+
+#    def test_block_switchcase(self):
+#        self.assertEqual(self.Goal._meta.get_field("block"), "")
+
+#    def test_quarter(self):
+#        self.assertEqual()
+
+    def test_weight_lower(self):
+        weight_invalid = Goal(weight=-1)
+        with self.assertRaises(ValidationError):
+            weight_invalid.full_clean()
+            weight_invalid.save()
+
+    def test_weight_higher(self):
+        weight_invalid = Goal(weight=101)
+        with self.assertRaises(ValidationError):
+            weight_invalid.full_clean()
+            weight_invalid.save()
