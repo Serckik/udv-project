b2c9ede68ccdc883b5bdb1c84d86338dec6d2533
Ramil Mavliutov <stirk2281337@gmail.com>
2023-04-14 16:53:47 +0500

    Fix get chats

--- goals/browse/views.py
+++ goals/browse/views.py
@@ -157,6 +157,8 @@ def get_chat(request):
     goal = Goal.objects.get(id=request.GET.get('goal_id'))
     chats = goal.chat_set.all()
     chats = [model_to_dict(x) for x in chats]
+    for chat in chats:
+        chat.append({'text': chat.message, 'time': chat.created_at, 'name': chat.owner_id.get_full_name()})
     return JsonResponse(chats, safe=False)
 
 
