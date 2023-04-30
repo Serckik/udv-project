13b2f45fb3aea638fc453191d078a5341b52fdd3
Ramil Mavliutov <stirk2281337@gmail.com>
2023-04-30 15:50:29 +0500

    add goal name to notifi

--- goals/users/views.py
+++ goals/users/views.py
@@ -5,6 +5,7 @@ from django.contrib.auth.decorators import login_required
 from django.http import HttpResponseRedirect, JsonResponse
 from .models import Notification
 from django.forms.models import model_to_dict
+from browse.models import Goal
 
 @login_required(login_url='/user/login/')
 @cache_control(no_cache=True, must_revalidate=True)
@@ -16,4 +17,6 @@ def logout_user(request):
 def get_notifications(request):
     notifi = Notification.objects.filter(user=request.user)
     notifi_list = list(notifi.values())
+    for notifi in notifi_list:
+        notifi['goal_name'] = Goal.objects.get(id=notifi['goal_id']).name
     return JsonResponse(notifi_list, safe=False)
