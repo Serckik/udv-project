b14fb8f5e1e866e9284aa1d028d52d1522d221ff
Serkik <63666583+Serckik@users.noreply.github.com>
2023-06-18 14:45:23 +0500

    улучшенный визуал формы добавить

--- goals/browse/static/browse/js/add.js
+++ goals/browse/static/browse/js/add.js
@@ -1,4 +1,4 @@
-import { FillForm, sleep, currentQuarter } from "./load.js"
+import { FillForm, sleep, currentQuarter, colors, opacityColors } from "./load.js"
 import { CardSend, CardNameError, FormChange, CardNameChange  } from "./openCard.js"
 import { Filter } from "./filter.js"
 
@@ -24,6 +24,8 @@ FillForm('add-form')
 $('.filter-container-element:nth-child(1)').remove();
 $('.filter-container-element:nth-child(1)').remove();
 $('.order-container-element.owner_id').remove();
+$('#add-form').attr('style', 'border-left:9px solid ' + colors['Оценка']);
+document.querySelector(':root').style.setProperty('--back-color', opacityColors['Оценка']);
 
 $(document).on('submit', '#add-form', async function(e){
     e.preventDefault();
@@ -43,27 +45,40 @@ $(document).on('submit', '#add-form', async function(e){
         request("POST", "/goal/add_goal", data)
         await sleep(sleepTime);
         Filter()
-        CardSend('add')
+        CardSend('add-container')
         $('#add-form #card-description').val('')
         $('#add-form #card-block').val('Оценка')
         $('#add-form #card-cvartal').val(currentQuarter)
         $('#add-form #card-category').val('Запланированная')
         $('#add-form #card-weight').val('0')
+        $('#add-form').attr('style', 'border-left:9px solid ' + colors['Оценка']);
+        document.querySelector(':root').style.setProperty('--back-color', opacityColors['Оценка']);
     }
     else{
-        CardNameError('add', 'card-name')
+        CardNameError('add-container', 'card-name')
     }
     $("#add-form #card-name").val('')
 })
 
 $(document).on('input', "#add-form #card-name", function(e){
-    CardNameChange('add', 'card-name', 'добавить')
+    CardNameChange('add-container', 'card-name', 'добавить')
 })
 
 $(document).on('input', "#add-form textarea", function(e){
-    FormChange('add', 'Добавить')
+    FormChange('add-container', 'Добавить')
 })
 
 $(document).on('change', "#add-form select", function(e){
-    FormChange('add', 'добавить')
+    FormChange('add-container', 'добавить')
 })
+
+$(document).on('change', "#add-form #card-block", function (e) { 
+    const block = $(this).val()
+    $('#add-form').attr('style', 'border-left:9px solid ' + colors[block]);
+    document.querySelector(':root').style.setProperty('--back-color', opacityColors[block]);
+ })
+
+ $(document).on('click', '.blur, .exit-icon svg', function(e){
+    const block = $('#add-form #card-block').val()
+    document.querySelector(':root').style.setProperty('--back-color', opacityColors[block]);
+});
